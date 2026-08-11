af222f0a6e878a7cdb74a003bec4280089797eb2
RaphaelEsch <esch.informatica@gmail.com>
2026-08-11 19:53:59 -0300

    Feat : "Adiciona busca de produto por ID e tratamento de 404".

--- src/main/java/com/br/estoque/controller/ProdutoController.java
+++ src/main/java/com/br/estoque/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.br.estoque.controller;
 import com.br.estoque.model.Produto;
 import com.br.estoque.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,9 +15,18 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	//Rota para buscar todos os produtos
 	@GetMapping
 	public List<Produto> listarTodos(){
 		return produtoRepository.findAll();
+	}
+	
+	//Rota para buscar produto por ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> buscarPorId(@PathVariable Long id){
+		return produtoRepository.findById(id)
+				.map(produto -> ResponseEntity.ok(produto))
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 
